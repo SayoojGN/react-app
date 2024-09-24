cdd84628e064314c14fbc51bcee971306a98d876
Sayooj <sayoojgeoninan2002@gmail.com>
2024-09-24 10:17:08 +0530

    First commit with initial files

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Message from "./Message";
+
+function App () {
+  return (
+    <div>
+      <Message />
+      <p>Welcome to my React App!</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Message.tsx
@@ -0,0 +1,6 @@
+function Message () {
+    const name = 'Sayooj'
+    return <h1>Hey this is {name}'s 1st react website</h1>
+}
+
+export default Message;
